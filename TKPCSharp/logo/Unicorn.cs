e3238c7a6085f59c3a84114b2445e3056cb75c6a
Lynn Langit <lynnlangit@live.com>
2020-09-28 23:26:10 +0000

    unicorn comments

--- TKPCSharp/logo/Unicorn.cs
+++ TKPCSharp/logo/Unicorn.cs
@@ -5,6 +5,9 @@ using src.main.java.org.teachingextentions.logo.utils.LineAndShapeUtils.Paintabl
 using src.main.java.org.teachingextentions.logo.utils.PuzzleUtils.Tile;
 using javax.swing.Star;
 using java.awt.Star;
+
+/// <img src="http://vignette2.wikia.nocookie.net/farmville/images/0/06/Sweet_Dreams_Unicorn-icon.png/revision/latest/scale-to-width/100?cb=20130802132644" style="text-align: left" alt="Unicorns are magical - like programming!"height=35 width=35 >
+/// The Unicorn lets you add Unicorns (rather than Turtles!) to your window
 public class Unicorn : Paintable {
     
     private int x;
@@ -28,9 +31,20 @@ public class Unicorn : Paintable {
 
     // TODO: error here
     //   return ObjectUtils.loadImage(Tile.class, (Animals.Unicorn + ".png"));
-    
+
     }
-    
+
+   /// <summary> Adds a Unicorn to the Program Window.  
+   /// <example>
+   ///  <code>  
+   ///      Unicorn.addTo(programWindow)}
+   ///  </code>
+   /// </example>
+   ///  <param>
+   ///      programWindow 
+   ///      is the current Program Window
+   ///  </param>
+   /// </summary>
     void AddTo(ProgramWindow programWindow) {
         programWindow.add(this);
     }
